0f25c25a58150f4bf9a39a3dc00563b92b0ab3ca
Rlvx <romain.412@gmail.com>
2020-09-04 10:41:55 +0200

    TD1_Ex9

---
+++ TD1/Ex9/src/Ex9.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Ex9 extends JFrame implements ActionListener{
+    JButton button1,button2 ;
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Bouton 1")){
+            JOptionPane.showMessageDialog(this,"Bouton 1");
+        }else{
+            JOptionPane.showMessageDialog(this,"Bouton 2");
+        }
+    }
+    Ex9(){
+        setTitle("Yes");
+        setBounds(400,200,502,250);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout(FlowLayout.CENTER,50,50));
+        button1 = new JButton("Bouton 1");
+        button2 = new JButton("Bouton 2");
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        add(button1);
+        add(button2);
+        pack();
+    }
+    public static void main(String[] args) {
+        Ex9 fen = new Ex9();
+    }
+}
