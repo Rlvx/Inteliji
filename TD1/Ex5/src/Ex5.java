19ba33bd169ad92bb0c9cba2efb4cb331d4d3e62
Rlvx <romain.412@gmail.com>
2020-09-04 10:01:49 +0200

    TD1_Ex9 simplifié

--- TD1/Ex5/src/Ex5.java
+++ TD1/Ex5/src/Ex5.java
@@ -1,38 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-public class Ex5 extends JFrame{
+public class Ex5 extends JFrame implements ActionListener{
     JButton button1,button2 ;
-    int number_clic = 0;
-    static class Fenetres{
-        JFrame fen ;
-        Fenetres(String title){
-            fen = new JFrame();
-            fen.setTitle(title);
-            fen.setBounds(400,200,502,250);
-            fen.setVisible(true);
-            fen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        }
-    }
-    class evenement_Clic implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if(e.getSource()==button1){
-                JOptionPane.showMessageDialog(new JFrame(),"Bouton 1");
-            }else{
-                JOptionPane.showMessageDialog(new JFrame(),"Bouton 2");
-            }
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Bouton 1")){
+                JOptionPane.showMessageDialog(this,"Bouton 1");
+        }else{
+                JOptionPane.showMessageDialog(this,"Bouton 2");
         }
     }
     Ex5(){
-        Fenetres ma_fenetre = new Fenetres("Test");
-        ma_fenetre.fen.setLayout(new FlowLayout(FlowLayout.CENTER,50,50));
+        setTitle("Yes");
+        setBounds(400,200,502,250);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout(FlowLayout.CENTER,50,50));
         button1 = new JButton("Bouton 1");
         button2 = new JButton("Bouton 2");
-        button1.addActionListener(new evenement_Clic());
-        button2.addActionListener(new evenement_Clic());
-        ma_fenetre.fen.add(button1);
-        ma_fenetre.fen.add(button2);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        add(button1);
+        add(button2);
         pack();
     }
     public static void main(String[] args) {
