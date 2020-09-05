e2127df0fe503b3343fc5f74dde2422332e6b098
Rlvx <romain.4212@gmail.com>
2020-09-05 14:00:02 +0200

    TD2_Ex3 version plus stylé

--- TD2/Ex3/src/TD2_Ex3.java
+++ TD2/Ex3/src/TD2_Ex3.java
@@ -1,48 +1,116 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class TD2_Ex3 extends JFrame {
+import java.util.ArrayList;
+//-----------------Déclaration d'une classe stockant le prix des plats et permetant d'instancier un JCheckBoxMenuItem et un JLabel avec comme nom le nom du plat---------------//
+class CheckBoxMenuItem{
+    JCheckBoxMenuItem item;
+    JLabel label ;
+    int value;
+    CheckBoxMenuItem(String name,int value){
+        item = new JCheckBoxMenuItem(name);
+        label = new JLabel("> "+name);
+        this.value = value;
+    }
+}
+public class TD2_Ex3 extends JFrame implements ActionListener {
+    JMenuBar bar_de_menu = new JMenuBar();
     JMenu menu1,menu2,menu3 ;
-    JMenuBar bar_de_menu ;
-    JCheckBoxMenuItem apéritif,entrée,plat,dessert,café,digestif;
-    JRadioButtonMenuItem espèce,chèque, carte_bleue, sur_place,emporter ;
+    JButton adition ;
+    JFrame fen_Adition ;
+    JLabel label,label2;
+    JTextField note;
+    JPanel haut,bas;
+    ArrayList<CheckBoxMenuItem> liste_plats;
+    ArrayList<JRadioButtonMenuItem> liste_paiement;
+    ArrayList<JRadioButtonMenuItem> liste_lieu;
+    int amount ;
     TD2_Ex3(){
+        //-----------------Param fenetre principale---------------//
         setTitle("Yes");
         setBounds(400,200,502,250);
+        setJMenuBar(bar_de_menu);
+        setLayout(new BorderLayout());
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        bar_de_menu = new JMenuBar();
+        //-----------------Instanciation elements graphiques---------------//
+        adition = new JButton("Adition");
         menu1 = new JMenu("Plats");
         menu2 = new JMenu("Paiement");
         menu3 = new JMenu("Lieu");
-        apéritif = new JCheckBoxMenuItem("apéritif");
-        entrée = new JCheckBoxMenuItem("entrée");
-        plat = new JCheckBoxMenuItem("plat");
-        dessert = new JCheckBoxMenuItem("dessert");
-        café = new JCheckBoxMenuItem("café");
-        digestif = new JCheckBoxMenuItem("digestif");
-        espèce = new JRadioButtonMenuItem("espèce");
-        chèque=new JRadioButtonMenuItem("chèque");
-        carte_bleue = new JRadioButtonMenuItem("carte bleue");
-        sur_place = new JRadioButtonMenuItem("sur place");
-        emporter = new JRadioButtonMenuItem("a emporter");
-        menu1.add(apéritif);
-        menu1.add(entrée);
-        menu1.add(plat);
-        menu1.add(dessert);
-        menu1.add(café);
-        menu1.add(digestif);
-        menu2.add(espèce);
-        menu2.add(chèque);
-        menu2.add(carte_bleue);
-        menu3.add(sur_place);
-        menu3.add(emporter);
+        //-----------------Affectation des menus à la barre de menu---------------//
         bar_de_menu.add(menu1);
         bar_de_menu.add(menu2);
         bar_de_menu.add(menu3);
-        setJMenuBar(bar_de_menu);
+        //-----------------Instanciation des Arraylistes d'items de menu---------------//
+        liste_plats = new ArrayList<>();
+        liste_paiement = new ArrayList<>();
+        liste_lieu = new ArrayList<>();
+        //-----------------Affectation des items de menu à leur Arrayliste respective---------------//
+        liste_plats.add(new CheckBoxMenuItem("apéritif",5));
+        liste_plats.add(new CheckBoxMenuItem("entrée",20));
+        liste_plats.add(new CheckBoxMenuItem("plat",30));
+        liste_plats.add(new CheckBoxMenuItem("dessert",10));
+        liste_plats.add(new CheckBoxMenuItem("café",3));
+        liste_plats.add(new CheckBoxMenuItem("digestif",3));
+        liste_paiement.add(new JRadioButtonMenuItem("espèce"));
+        liste_paiement.add(new JRadioButtonMenuItem("chèque"));
+        liste_paiement.add(new JRadioButtonMenuItem("carte bleue"));
+        liste_lieu.add(new JRadioButtonMenuItem("sur place"));
+        liste_lieu.add(new JRadioButtonMenuItem("a emporter"));
+        //-----------------Affectation des items de menu à leur menu respectif---------------//
+        for(CheckBoxMenuItem items : liste_plats){
+            menu1.add(items.item);
+        }
+        for(JRadioButtonMenuItem items : liste_paiement){
+            menu2.add(items);
+        }
+        for(JRadioButtonMenuItem items : liste_lieu){
+            menu3.add(items);
+        }
+        //-----------------Ajout d'un ecouteur d'evenement a l'élément graphique de type bouton---------------//
+        adition.addActionListener(this);
+        //-----------------Ajout d'un element graphique de type bouton au layout---------------//
+        add(adition,BorderLayout.CENTER);
     }
     public static void main(String[] args) {
         TD2_Ex3 fen = new TD2_Ex3();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //-----------------Instaciation et Paramétrage fenetre Adition---------------//
+        fen_Adition = new JFrame();
+        fen_Adition.setTitle("Adition");
+        fen_Adition.setBounds(400,200,300,450);
+        fen_Adition.setLayout(new BorderLayout());
+        fen_Adition.setVisible(true);
+        //-----------------Instanciation elements graphiques---------------//
+        haut = new JPanel();
+        bas = new JPanel();
+        label = new JLabel("Vous avez commandé :");
+        label2 = new JLabel("La note est de :");
+        note = new JTextField();
+        //-----------------Affectation d'un layout manager au deux panel composant la fenetre adition---------------//
+        haut.setLayout(new GridLayout(7,0));
+        bas.setLayout(new GridLayout(2,0));
+        fen_Adition.add(haut,BorderLayout.CENTER);
+        fen_Adition.add(bas,BorderLayout.SOUTH);
+        //-----------------Mise en readonly du textarea où est inscrit le montant de la note---------------//
+        note.setEnabled(false);
+        //-----------------ajout d'un label dans le panel du "haut"---------------//
+        haut.add(label);
+        //-----------------Boucle renvoyant tous les plats coché et calcul le montant de la note---------------//
+        amount = 0 ;
+        for(CheckBoxMenuItem items : liste_plats){
+            if(items.item.isSelected()){
+                haut.add(items.label);
+                amount+=items.value;
+            }
+        }
+        //-----------------ajout des derniers éléments graphiques---------------//
+        bas.add(label2);
+        note.setText(amount+" €");
+        note.setDisabledTextColor(Color.black);
+        bas.add(note);
     }
 }
