c52d6cfc0c98fd1361b123e5bf78709b6a2032bb
Rlvx <romain.412@gmail.com>
2020-09-08 17:13:56 +0200

    Modifié par Nunes en personne

--- TD2/Ex3/src/TD2_Ex3.java
+++ TD2/Ex3/src/TD2_Ex3.java
@@ -14,13 +14,14 @@ class CheckBoxMenuItem{
     }
 }
 public class TD2_Ex3 extends JFrame implements ActionListener {
-    JMenuBar bar_de_menu = new JMenuBar();
+    JMenuBar bar_de_menu;
     JMenu menu1,menu2,menu3 ;
     JButton adition ;
     JFrame fen_Adition ;
     JLabel label,label2;
     JTextField note;
     JPanel haut,bas;
+    ButtonGroup groupepaie,lieuconso;
     ArrayList<CheckBoxMenuItem> liste_plats;
     ArrayList<JRadioButtonMenuItem> liste_paiement;
     ArrayList<JRadioButtonMenuItem> liste_lieu;
@@ -29,9 +30,10 @@ public class TD2_Ex3 extends JFrame implements ActionListener {
         //-----------------Param fenetre principale---------------//
         setTitle("Yes");
         setBounds(400,200,502,250);
+        bar_de_menu=new JMenuBar();
         setJMenuBar(bar_de_menu);
-        setLayout(new BorderLayout());
-        setVisible(true);
+        //setLayout(new BorderLayout());
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //-----------------Instanciation elements graphiques---------------//
         adition = new JButton("Adition");
@@ -57,21 +59,26 @@ public class TD2_Ex3 extends JFrame implements ActionListener {
         liste_paiement.add(new JRadioButtonMenuItem("chèque"));
         liste_paiement.add(new JRadioButtonMenuItem("carte bleue"));
         liste_lieu.add(new JRadioButtonMenuItem("sur place"));
-        liste_lieu.add(new JRadioButtonMenuItem("a emporter"));
+        liste_lieu.add(new JRadioButtonMenuItem("à emporter"));
+        groupepaie=new ButtonGroup();
+        lieuconso=new ButtonGroup();
         //-----------------Affectation des items de menu à leur menu respectif---------------//
         for(CheckBoxMenuItem items : liste_plats){
             menu1.add(items.item);
         }
         for(JRadioButtonMenuItem items : liste_paiement){
             menu2.add(items);
+            groupepaie.add(items);
         }
         for(JRadioButtonMenuItem items : liste_lieu){
             menu3.add(items);
+            lieuconso.add(items);
         }
         //-----------------Ajout d'un ecouteur d'evenement a l'élément graphique de type bouton---------------//
         adition.addActionListener(this);
         //-----------------Ajout d'un element graphique de type bouton au layout---------------//
         add(adition,BorderLayout.CENTER);
+        setVisible(true);
     }
     public static void main(String[] args) {
         TD2_Ex3 fen = new TD2_Ex3();
@@ -83,7 +90,7 @@ public class TD2_Ex3 extends JFrame implements ActionListener {
         fen_Adition.setTitle("Adition");
         fen_Adition.setBounds(400,200,300,450);
         fen_Adition.setLayout(new BorderLayout());
-        fen_Adition.setVisible(true);
+
         //-----------------Instanciation elements graphiques---------------//
         haut = new JPanel();
         bas = new JPanel();
@@ -91,8 +98,8 @@ public class TD2_Ex3 extends JFrame implements ActionListener {
         label2 = new JLabel("La note est de :");
         note = new JTextField();
         //-----------------Affectation d'un layout manager au deux panel composant la fenetre adition---------------//
-        haut.setLayout(new GridLayout(7,0));
-        bas.setLayout(new GridLayout(2,0));
+        haut.setLayout(new GridLayout(7,1));
+        bas.setLayout(new GridLayout(2,1));
         fen_Adition.add(haut,BorderLayout.CENTER);
         fen_Adition.add(bas,BorderLayout.SOUTH);
         //-----------------Mise en readonly du textarea où est inscrit le montant de la note---------------//
@@ -112,5 +119,6 @@ public class TD2_Ex3 extends JFrame implements ActionListener {
         note.setText(amount+" €");
         note.setDisabledTextColor(Color.black);
         bas.add(note);
+        fen_Adition.setVisible(true);
     }
 }
